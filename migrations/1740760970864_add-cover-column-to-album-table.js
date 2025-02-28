a1a05849c6b28814a227d77da29c4ff70aff0de6
fikriomar16 <fikriomar60@gmail.com>
2025-02-28 23:43:57 +0700

    [UPDATE] Migration

---
+++ migrations/1740760970864_add-cover-column-to-album-table.js
@@ -0,0 +1,21 @@
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.up = (pgm) => {
+  pgm.addColumn('albums', {
+    cover: {
+      type: 'TEXT',
+    },
+  });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.down = (pgm) => {
+  pgm.dropColumn('albums', 'cover');
+};
