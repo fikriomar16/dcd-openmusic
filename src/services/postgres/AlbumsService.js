5e0f21a31f88f85d73f79fa5d7a74dde03b3e066
fikriomar16 <fikriomar60@gmail.com>
2025-03-02 16:18:09 +0700

    [UPDATE] Check rows

--- src/services/postgres/AlbumsService.js
+++ src/services/postgres/AlbumsService.js
@@ -97,7 +97,7 @@ class AlbumsService {
     };
     const result = await this._pool.query(query);
 
-    if (!result.rows.length) {
+    if (!result.rowCount) {
       throw new NotFoundError(
         'Gagal memperbarui cover album. Id tidak ditemukan',
       );
